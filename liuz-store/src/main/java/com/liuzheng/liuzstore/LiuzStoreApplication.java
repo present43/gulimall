0317feb3310ad2b1a55b50eb25fd32e55314fe25
present43 <2351810431@qq.com>
2020-09-27 09:27:10 +0800

    基础目录结构

---
+++ liuz-store/src/main/java/com/liuzheng/liuzstore/LiuzStoreApplication.java
@@ -0,0 +1,13 @@
+package com.liuzheng.liuzstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuzStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuzStoreApplication.class, args);
+    }
+
+}
