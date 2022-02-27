fc6da3e595d7f8039968e671f389b3e7e33ff3e4
“lz” <501146993@qq.com>
2022-02-27 18:12:53 +0800

    添加oss图片上传

---
+++ three-server/src/test/java/com/liuzheng/gulimall/server/ThreeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuzheng.gulimall.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
